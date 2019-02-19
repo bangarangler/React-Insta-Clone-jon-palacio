e2c80b97bb4d05fc7b4ed4d7620e3bc64ac10d0f
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-18 19:11:31 -0500

    Added proptypes for object data

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.scss";
 import dummyData from "./dummy-data";
+import PropTypes from "prop-types";
 
 import SearchBar from "./components/SearchBar/SearchBar";
 import PostContainer from "./components/PostContainer/PostContainer";
@@ -20,5 +21,23 @@ class App extends Component {
     );
   }
 }
+
+dummyData.propTypes = {
+  dummyData: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      thumbnailUrl: PropTypes.string,
+      imageUrl: PropTypes.string,
+      likes: PropTypes.number,
+      timestamp: PropTypes.string,
+      comments: PropTypes.arrayOf(
+        PropTypes.shape({
+          username: PropTypes.string,
+          text: PropTypes.string
+        })
+      )
+    })
+  )
+};
 
 export default App;
